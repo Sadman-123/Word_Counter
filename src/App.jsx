3f938b9ec0b7b1165c4fa7312756fa216995a72b
Sadman-123 <19201103123@cse.bubt.edu.bd>
2024-03-29 06:30:33 +0600

    Some changes

--- src/App.jsx
+++ src/App.jsx
@@ -3,13 +3,15 @@ import Heading from './component/Heading'
 import Input from './component/Input'
 import Nav from './pages/Nav'
 import Clock from './pages/Clock'
+import Color from './pages/Color'
 const App = () => {
   return (
     <div>
       <Nav/>
       {/* <Heading/>
-      <Input/> */}
-      <Clock/>
+      <Input/>
+      <Clock/> */}
+      <Color/>
     </div>
   )
 }
---
+++ src/pages/Color.jsx
@@ -0,0 +1,17 @@
+import React from 'react'
+import '../CSS/style.css'
+const Color = () => {
+  return (
+    <div className='clr'>
+<h1>Color Picker</h1>
+<div className="container">
+<div className="clrcanvas">
+    <h2>Color Code</h2>
+    <input type="color" name="" id="" />
+</div>
+</div>
+    </div>
+  )
+}
+
+export default Color
