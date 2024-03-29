8c7c08f54b27bc14242cefa7976cb481d31b18ac
Sadman-123 <19201103123@cse.bubt.edu.bd>
2024-03-29 06:44:12 +0600

    some fix

--- src/App.jsx
+++ src/App.jsx
@@ -1,17 +1,11 @@
 import React from 'react'
 import Heading from './component/Heading'
 import Input from './component/Input'
-import Nav from './pages/Nav'
-import Clock from './pages/Clock'
-import Color from './pages/Color'
 const App = () => {
   return (
     <div>
-      <Nav/>
-      {/* <Heading/>
+      <Heading/>
       <Input/>
-      <Clock/> */}
-      <Color/>
     </div>
   )
 }
--- src/utility/Master.jsx
+++ src/utility/Master.jsx
@@ -5,9 +5,11 @@ import Color from '../pages/Color'
 import Clock from '../pages/Clock'
 import About from '../pages/About'
 import App from '../App'
+import Nav from '../pages/Nav'
 const Master = () => {
   return (
     <div>
+      <Nav/>
         <BrowserRouter>
         <Routes>
             <Route path='/' element={<Splash/>}/>
