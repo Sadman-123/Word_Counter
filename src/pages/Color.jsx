fc2020fd05aaf485d2bbcae04959e24b2c6a7343
Sadman-123 <19201103123@cse.bubt.edu.bd>
2024-03-29 06:34:46 +0600

    Color picker done

--- src/pages/Color.jsx
+++ src/pages/Color.jsx
@@ -1,13 +1,19 @@
-import React from 'react'
+import React, { useState } from 'react'
 import '../CSS/style.css'
 const Color = () => {
+    const [clr,setclr]=useState("Color Code");
   return (
     <div className='clr'>
 <h1>Color Picker</h1>
 <div className="container">
-<div className="clrcanvas">
-    <h2>Color Code</h2>
-    <input type="color" name="" id="" />
+<div className="clrcanvas" id='clrcanvas'>
+    <h2 id='clr_code'>{clr}</h2>
+    <input type="color" name="" id="" onChange={(e)=>{
+        setclr(e.target.value)
+        let x=document.getElementById('clr_code')
+        let x_bg=document.getElementById('clrcanvas')
+        x_bg.style.backgroundColor=clr
+    }} />
 </div>
 </div>
     </div>
