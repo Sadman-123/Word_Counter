06d0ae61d1e8d4b8063f9ee67ea8d32c805d105c
Sadman-123 <19201103123@cse.bubt.edu.bd>
2024-03-29 06:12:21 +0600

    some css

--- src/pages/Nav.jsx
+++ src/pages/Nav.jsx
@@ -3,11 +3,10 @@ import '../CSS/style.css'
 const Nav = () => {
   return (
     <div className='navbar'>
-<a class="active" href="#">Home</a>
-  <a href="#">About</a>
-  <a href="#">Services</a>
-  <a href="#">Portfolio</a>
-  <a href="#">Contact</a>
+<a class="active" href="#"><i class="fa-solid fa-house"></i>Home</a>
+  <a href="#"><i class="fa-solid fa-clock"></i>Clock</a>
+  <a href="#"><i class="fa-solid fa-palette"></i>Colors</a>
+  <a href="#"><i class="fa-solid fa-circle-info"></i>About</a>
     </div>
   )
 }
