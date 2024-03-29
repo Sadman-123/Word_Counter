04797ad6cfb9a721959f23575e0028a4086a7324
Sadman-123 <19201103123@cse.bubt.edu.bd>
2024-03-29 06:04:31 +0600

    Added Navbar and some css

---
+++ src/pages/Nav.jsx
@@ -0,0 +1,15 @@
+import React from 'react'
+import '../CSS/style.css'
+const Nav = () => {
+  return (
+    <div className='navbar'>
+<a class="active" href="#">Home</a>
+  <a href="#">About</a>
+  <a href="#">Services</a>
+  <a href="#">Portfolio</a>
+  <a href="#">Contact</a>
+    </div>
+  )
+}
+
+export default Nav
