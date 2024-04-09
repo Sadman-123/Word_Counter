1c82d7156e3b2750415c2b9306a0eb290721a61d
Sadman-123 <19201103123@cse.bubt.edu.bd>
2024-04-09 09:15:17 +0600

    verify icon color

--- src/component/Heading.jsx
+++ src/component/Heading.jsx
@@ -6,7 +6,7 @@ const Heading = () => {
         <h1>
         <i class="fa-solid fa-calculator" id='lol'></i>  
           
-           Word Counter
+           Word Counter <i class="fa-solid fa-circle-check" id='verifylogo'></i>
         </h1>
     </div>
   )
