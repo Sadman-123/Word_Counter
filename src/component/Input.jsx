c01c1887b756f6b8235891a35a154f6dea512a0d
Sadman-123 <19201103123@cse.bubt.edu.bd>
2024-04-09 09:07:15 +0600

    Some Color Change css

--- src/component/Input.jsx
+++ src/component/Input.jsx
@@ -4,12 +4,15 @@ import '../CSS/style.css'
 const Input = () => {
   const [word,setword]=useState("");
   return (
-    <div className='inpt'>
+   <>
+      <div className="hlw">
         <input type="text" name="" id="" placeholder='Enter Word' onChange={(e)=>{
           setword(e.target.value)
           }}/>
+          <i class="fa-solid fa-eye-slash"></i>
+      </div>
          <Output val={word.length}/>
-    </div>
+   </>
   )
 }
 
